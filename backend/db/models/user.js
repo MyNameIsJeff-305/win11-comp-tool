c9c94ff2fd068f543536760155a789c4313d1f2a
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-07-29 09:27:44 -0400

    Added User Model and Migrations

---
+++ backend/db/models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+
+    static associate(models) {
+
+    }
+  }
+  User.init({
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: { exclude: ['hashedPassword', 'createdAt', 'updatedAt'] } 
+    },
+  });
+  return User;
+};
