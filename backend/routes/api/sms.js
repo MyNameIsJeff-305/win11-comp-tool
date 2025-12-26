10c734094aa2f81ab99b3eee9c4e08197785c49b
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-12-26 13:02:28 -0500

    Testing new sms endpoint

--- backend/routes/api/sms.js
+++ backend/routes/api/sms.js
@@ -28,7 +28,7 @@ async function findBackupTicket(phone) {
 
     console.log("SEARCHING FOR THE REQUESTER")
     //Search for a requester with the phone number
-    const fsRequesterResponse = await fs.get(`/requesters?query=work_phone_number:${phone}`)
+    const fsRequesterResponse = await fs.get(`/requesters?query=work_phone_number:${encodeURIComponent(phone)}`);
     console.log("REQUESTER SEARCH RESPONSE:", fsRequesterResponse.data);
     const requester = fsRequesterResponse.data.requesters?.[0];
 
